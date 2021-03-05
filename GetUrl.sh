2738071b8cf4d7e07b342a065c131ab66f5ba5e6
NolanB <NolanB.titi@toto.fr>
2021-03-05 11:23:46 +0100

    add regex

--- GetUrl.sh
+++ GetUrl.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-
-if [ $0 -eq 0 ]; then 
-	echo "Missing one argument: The URL to process."
-	#use  an help  function to display doc .
-	exit 1
+if [ $# -eq 0 ]; then
+    echo "Missing one argument: the URL to process."
+    # use an help function to display doc.
+    exit 1
 fi
-
 
 if [ $# -gt 1 ]; then
-        echo "To many arguments."
-        #use  an help  function to display doc .
-        exit 2
+    echo "Too many arguments."
+    # use an help function to display doc.
+    exit 2
 fi
 
+URL_REGEX='((https?:)?\/\/)?((([a-z0-9\-_]+\.)+[a-z]{2,})|(([0-9]{1,3}\.){3}[0-9]{1,3}))(\/[a-z0-9\-_]+)*\/?'
 
-wget -q -O  - $1 
+COUNT=`echo "$1" | grep -i -E -o "$URL_REGEX" | wc -l`
+
+if [ $COUNT -eq 0 ]; then
+    echo "Not a valid URL."
+    exit 3
+fi
+
+wget -q -O - "$1" | grep -i -E -o "$URL_REGEX"
